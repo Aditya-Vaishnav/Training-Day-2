c75e98f1237d09048f280ead186ae2f7138838ab
Aditya-Vaishnav <avaishnav@grepruby.com>
2016-06-23 14:06:17 +0530

    Well formatted output

--- Purchase.rb
+++ Purchase.rb
@@ -1,13 +1,16 @@
 class Purchase
    @@purchases = Array.new
-   @@grand_total =0
+   @@grand_total = 0
+   @@no_of_writes = 0
    @@tax_free = ["Book","Chocolate Box","Chocolate Bar","Headache Pills"]
    def initialize(name,price,qty,imported)
 	@name = name
 	@price = price
 	@qty = qty
 	@imported =imported
-	if(!@@tax_free.include?(name))	
+	if(!@@tax_free.include?(name))
+	puts "calculating tax"
+	sleep 1	
 	@tax = Tax.calcTax(@price,@qty)		 		#Automated Calculation of Tax
 	else
 	@tax = 0.0
@@ -15,7 +18,7 @@ class Purchase
 	if(@imported.eql?("y"))
 	@imp_duty = Tax.calcImportDuty(@price,@qty)		#Automated Calculation of import duty
 	else
-	@imp_duty = 0	
+	@imp_duty = 0.0	
 	end
 	@total_price = ((@price*@qty)+@tax+@imp_duty).round(2)  #Total price with inclusive of all taxes
 	@@purchases << self
@@ -35,10 +38,25 @@ class Purchase
    def get_total
 	@@total
    end
+   def get_initial_formatting
+	CSV.open("outputFile.csv","w+") do |csv|
+	csv << ["Product Name","Price","Quantity","Import Status","Tax","Import Duty","Total Cost"]	
+	csv << []
+    	end
+   end
    def write_to_csv						#output to CSV
-	CSV.open("outputFile.csv","w") do |csv|
+	if(@@no_of_writes==0)					#prepare file for first use
+	   get_initial_formatting
+	end		
+	@@no_of_writes+=1
+	CSV.open("outputFile.csv","a+") do |csv|
 	csv << self.to_a
 	self.setGrandTotal(self.total_price)
+	if(@@no_of_writes == @@purchases.size)
+	   puts "done"
+	   csv << []
+	   csv<< ["","","","","","Grand Total",@@grand_total]
+	end
 	end
    end
 end
